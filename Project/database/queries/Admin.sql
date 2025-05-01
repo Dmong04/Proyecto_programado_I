dc3003971ef74ed4579ded22374c3846c11ef723
Pinaruma <juan98xxxx98@gmail.com>
2025-04-30 20:51:25 -0600

    Update Admin.sql

--- Project/database/queries/Admin.sql
+++ Project/database/queries/Admin.sql
@@ -1,1 +1,17 @@
--- 
+-- name: GetAll :many
+SELECT * FROM Administrador;
+
+-- name: GetAdministradorById :one
+SELECT * FROM Administrador WHERE idAdministrador = ?;
+
+-- name: CreateAdministrador :execresult
+INSERT INTO Administrador (nombre, usuario, contraseña)
+VALUES (?, ?, ?);
+
+-- name: UpdateAdministrador :exec
+UPDATE Administrador
+SET nombre = ?, usuario = ?, contraseña = ?
+WHERE idAdministrador = ?;
+
+-- name: DeleteAdministrador :exec
+DELETE FROM Administrador WHERE idAdministrador = ?;
