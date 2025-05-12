634a08ef6085afea2194ca61d794ff87c9ccb9b7
Dereck Monge Chaves <168316345+Dmong04@users.noreply.github.com>
2025-05-12 12:04:48 -0600

    Update Server.go

--- Project/api/Server.go
+++ Project/api/Server.go
@@ -24,7 +24,7 @@ func NewServer(dbtx *dto.DbTransaction) (*Server, error) {
 	}
 	router := gin.Default()
 	// Rutas (Endpoints) De la API
-	// CRUD Aministrador
+	// CRUD Aministrador (Funciona)
 	router.GET("/api/v1/Admin/all", server.GetAllAdmins)
 	router.POST("/api/v1/Admin", server.CreateAdmin)
 	router.GET("/api/v1/Admin/:id", server.GetAdminByID)
@@ -33,7 +33,7 @@ func NewServer(dbtx *dto.DbTransaction) (*Server, error) {
 	router.PATCH("/api/v1/Admin/update/password/:id", server.UpdateAdminPassword)
 	router.DELETE("/api/v1/Admin/delete/:id", server.DeleteAdmin)
 	router.DELETE("/api/v1/Admin/delete/name/:name", server.DeleteAdminByName)
-	// CRUD Pasajeros
+	// CRUD Pasajeros (Funciona)
 	router.GET("/api/v1/Passengers/all", server.GetAllPassengers)
 	router.POST("/api/v1/Passengers", server.CreatePassenger)
 	router.GET("/api/v1/Passengers/:id", server.GetPassengersById)
@@ -41,7 +41,7 @@ func NewServer(dbtx *dto.DbTransaction) (*Server, error) {
 	router.GET("/api/v1/Passengers/name/:name", server.GetPassengersByName)
 	router.PATCH("/api/v1/Passengers/update/:id", server.UpdatePassengers)
 	router.DELETE("/api/v1/Passengers/delete/:id", server.DeletePassenger)
-	// CRUD DetalleViajes
+	// CRUD DetalleViajes (Funciona)
 	router.GET("/api/v1/Details/all", server.getAllDetails)
 	router.POST("/api/v1/Details", server.CreateDetail)
 	router.GET("/api/v1/Details/:id", server.getDetailsByID)
@@ -53,7 +53,7 @@ func NewServer(dbtx *dto.DbTransaction) (*Server, error) {
 	router.GET("/api/v1/History/:id", server.GetHistoryByID)
 	router.PATCH("/api/v1/History/update/:id", server.UpdateHistory)
 	router.DELETE("/api/v1/History/delete/:id", server.DeleteHistory)
-	//CRUD Proveedor
+	//CRUD Proveedor (Funciona)
 	router.GET("/api/v1/Provider/all", server.GetAllProviders)
 	router.POST("/api/v1/Provider", server.CreateProvider)
 	router.GET("/api/v1/Provider/:id", server.GetProviderByID)
@@ -62,7 +62,7 @@ func NewServer(dbtx *dto.DbTransaction) (*Server, error) {
 	router.PATCH("/api/v1/Provider/update/name/:name", server.UpdateProviderByName)
 	router.DELETE("/api/v1/Provider/delete/:id", server.DeleteProvider)
 	router.DELETE("/api/v1/Provider/delete/name/:name", server.DeleteProviderByName)
-	// CRUD Client
+	// CRUD Client (Funciona)
 	router.POST("/api/v1/Client", server.CreateClient)
 	router.GET("/api/v1/Client/id/:id", server.GetClientByID)
 	router.GET("/api/v1/Client/all", server.GetAllClients)
@@ -78,7 +78,7 @@ func NewServer(dbtx *dto.DbTransaction) (*Server, error) {
 	router.GET("/api/v1/Client/Phones/all", server.GetAllClientPhones)
 	router.PATCH("/api/v1/Client/Phones/update/:id", server.UpdateClientPhones)
 	router.DELETE("/api/v1/Client/Phones/delete/:id", server.DeleteClientPhones)
-	// CRUD Travel
+	// CRUD Travel (Funciona)
 	router.POST("/api/v1/Travel", server.CreateTravel)
 	router.GET("/api/v1/Travel/all", server.GetAllTravels)
 	router.GET("/api/v1/Travel/:id", server.GetTravelById)
